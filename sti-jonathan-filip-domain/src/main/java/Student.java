4faa6d1409862944bc5eff2e69dbdbbd6dfbd4fc
Filip Mathsson <pigfof@gmail.com>
2022-01-31 14:28:53 +0100

    personenummer update

--- sti-jonathan-filip-domain/src/main/java/Student.java
+++ sti-jonathan-filip-domain/src/main/java/Student.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Student {
     private String fName;
     private String eName;
-    private int personNummer;
+    private String personNummer;
     private ArrayList<Course> courseList = new ArrayList<>();
 
-    public Student(String fName, String eName, int personNummer, ArrayList<Course> courseList){
+    public Student(String fName, String eName, String personNummer, ArrayList<Course> courseList){
         this.fName = fName;
         this.eName = eName;
         this.personNummer = personNummer;
--- sti-jonathan-filip-domain/src/main/java/Teacher.java
+++ sti-jonathan-filip-domain/src/main/java/Teacher.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class Teacher {
     private String fName;
     private String eName;
-    private int personNummer;
+    private String personNummer;
     private ArrayList<Course> courseList = new ArrayList<>();
     private int hourSalary;
 
-    public Teacher(String fName, String eName, int personNummer, ArrayList<Course> courseList, int hourSalary){
+    public Teacher(String fName, String eName, String personNummer, ArrayList<Course> courseList, int hourSalary){
         this.fName = fName;
         this.eName = eName;
         this.personNummer = personNummer;
